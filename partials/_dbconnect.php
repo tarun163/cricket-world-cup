894cb569b4be13513835582d8ef8e88e72db2ba4
smeer007 <sameeruikey16@gmail.com>
2022-05-29 17:13:56 +0530

    database added

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "users"; 
+$server = "ec2-54-211-255-161.compute-1.amazonaws.com";
+$username = "hwloqbddjiueif";
+$password = "db53d65bec4f99057ec7d427b62eb6b62d267851d17d6749194bcc0d427c1661";
+$database = "d963ac4fljnjd2"; 
 
 $conn = mysqli_connect($server, $username, $password, $database);
 if (!$conn){
